9312a0a1c7deac33e02c0e0795b45a1a3067cf6a
mingyang <15010029361@163.com>
2021-11-25 22:06:24 +0800

    add first homework

---
+++ firstclass/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("-----小诗-----")
+	fmt.Println(` 
+窗前明月光，
+疑是地上霜。
+举头望明月，
+低头思故乡。`)
+	fmt.Println("-----五角星-----")
+	fmt.Println("        *  ")
+	fmt.Println("       *  *")
+	fmt.Println("*  *  *     *  *  *")
+	fmt.Println("   *           *")
+	fmt.Println("     *      *")
+	fmt.Println("   *  *    *  *")
+	fmt.Println("  *            *")
+
+
+}
