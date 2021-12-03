8a82ffb05b21cffead2e3662d43604e67b7ef0d7
mingyang <15010029361@163.com>
2021-12-03 23:14:09 +0800

    提交第三节作业

--- thirdclass/cal_somePerson/main.go
+++ thirdclass/cal_somePerson/main.go
@@ -2,161 +2,147 @@ package main
 
 import "fmt"
 
-func testFat(name string,sex string, bmi float64, age int){
+func testFat(name string, sex string, bmi float64, age int) float64 {
 	var sexWeight float64
 	if sex == "男" {
 		sexWeight = 1
 	} else {
 		sexWeight = 0
 	}
-	var fatRate float64 = (1.2*bmi + 0.23*float64(age) - 5.4 - 10.8*sexWeight)/100
+	var fatRate float64 = (1.2*bmi + 0.23*float64(age) - 5.4 - 10.8*sexWeight) / 100
 	fmt.Println("姓名：", name)
-	fmt.Printf("体脂率是：%.2f\n",fatRate)
+	fmt.Printf("体脂率是：%.2f\n", fatRate)
 
 	if sex == "男" {
-		if age >=18 && age <=39{
-			if fatRate <=0.1{
+		if age >= 18 && age <= 39 {
+			if fatRate <= 0.1 {
 				fmt.Println("目前偏瘦，多吃多锻炼")
-			}else if fatRate>0.1&&fatRate<=0.16{
+			} else if fatRate > 0.1 && fatRate <= 0.16 {
 				fmt.Println("目前标准，太棒了，要保持哦")
-			}else if fatRate>0.16&&fatRate<=0.21 {
+			} else if fatRate > 0.16 && fatRate <= 0.21 {
 				fmt.Println("目前偏重，吃完饭多散散步，消化消化")
-			}else if fatRate>0.21&&fatRate<=0.26{
+			} else if fatRate > 0.21 && fatRate <= 0.26 {
 				fmt.Println("目前肥胖，开始锻炼锻炼吧～胖砸")
-			}else{
+			} else {
 				fmt.Println("目前严重肥胖！！！管住嘴，迈开腿，开始减肥吧！！！")
 			}
-		}else if age >=40 &&age <=59{
-			if fatRate <=0.11{
+		} else if age >= 40 && age <= 59 {
+			if fatRate <= 0.11 {
 				fmt.Println("目前偏瘦，多吃多锻炼")
-			}else if fatRate>0.11&&fatRate<=0.17{
+			} else if fatRate > 0.11 && fatRate <= 0.17 {
 				fmt.Println("目前标准，太棒了，要保持哦")
-			}else if fatRate>0.17&&fatRate<=0.22 {
+			} else if fatRate > 0.17 && fatRate <= 0.22 {
 				fmt.Println("目前偏重，吃完饭多散散步，消化消化")
-			}else if fatRate>0.22&&fatRate<=0.27{
+			} else if fatRate > 0.22 && fatRate <= 0.27 {
 				fmt.Println("目前肥胖，开始锻炼锻炼吧～胖砸")
-			}else{
+			} else {
 				fmt.Println("目前严重肥胖！！！管住嘴，迈开腿，开始减肥吧！！！")
 			}
-		}else if age>=60{
-			if fatRate <=0.13{
+		} else if age >= 60 {
+			if fatRate <= 0.13 {
 				fmt.Println("目前偏瘦，多吃多锻炼")
-			}else if fatRate>0.13&&fatRate<=0.19{
+			} else if fatRate > 0.13 && fatRate <= 0.19 {
 				fmt.Println("目前标准，太棒了，要保持哦")
-			}else if fatRate>0.19&&fatRate<=0.24 {
+			} else if fatRate > 0.19 && fatRate <= 0.24 {
 				fmt.Println("目前偏重，吃完饭多散散步，消化消化")
-			}else if fatRate>0.24&&fatRate<=0.29{
+			} else if fatRate > 0.24 && fatRate <= 0.29 {
 				fmt.Println("目前肥胖，开始锻炼锻炼吧～胖砸")
-			}else{
+			} else {
 				fmt.Println("目前严重肥胖！！！管住嘴，迈开腿，开始减肥吧！！！")
 			}
-		}else{
+		} else {
 			fmt.Println("暂时不支持计算未成年人的体脂率～")
 		}
 	} else {
-		if age >=18 && age <=39{
-			if fatRate <=0.2{
+		if age >= 18 && age <= 39 {
+			if fatRate <= 0.2 {
 				fmt.Println("目前偏瘦，多吃多锻炼")
-			}else if fatRate>0.2&&fatRate<=0.27{
+			} else if fatRate > 0.2 && fatRate <= 0.27 {
 				fmt.Println("目前标准，太棒了，要保持哦")
-			}else if fatRate>0.27&&fatRate<=0.34 {
+			} else if fatRate > 0.27 && fatRate <= 0.34 {
 				fmt.Println("目前偏重，吃完饭多散散步，消化消化")
-			}else if fatRate>0.34&&fatRate<=0.39{
+			} else if fatRate > 0.34 && fatRate <= 0.39 {
 				fmt.Println("目前肥胖，开始锻炼锻炼吧～胖砸")
-			}else{
+			} else {
 				fmt.Println("目前严重肥胖！！！管住嘴，迈开腿，开始减肥吧！！！")
 			}
-		}else if age >=40 &&age <=59{
-			if fatRate <=0.21{
+		} else if age >= 40 && age <= 59 {
+			if fatRate <= 0.21 {
 				fmt.Println("目前偏瘦，多吃多锻炼")
-			}else if fatRate>0.21&&fatRate<=0.28{
+			} else if fatRate > 0.21 && fatRate <= 0.28 {
 				fmt.Println("目前标准，太棒了，要保持哦")
-			}else if fatRate>0.28&&fatRate<=0.35 {
+			} else if fatRate > 0.28 && fatRate <= 0.35 {
 				fmt.Println("目前偏重，吃完饭多散散步，消化消化")
-			}else if fatRate>0.35&&fatRate<=0.41{
+			} else if fatRate > 0.35 && fatRate <= 0.41 {
 				fmt.Println("目前肥胖，开始锻炼锻炼吧～胖砸")
-			}else{
+			} else {
 				fmt.Println("目前严重肥胖！！！管住嘴，迈开腿，开始减肥吧！！！")
 			}
-		}else if age>=60{
-			if fatRate <=0.22{
+		} else if age >= 60 {
+			if fatRate <= 0.22 {
 				fmt.Println("目前偏瘦，多吃多锻炼")
-			}else if fatRate>0.22&&fatRate<=0.29{
+			} else if fatRate > 0.22 && fatRate <= 0.29 {
 				fmt.Println("目前标准，太棒了，要保持哦")
-			}else if fatRate>0.29&&fatRate<=0.36 {
+			} else if fatRate > 0.29 && fatRate <= 0.36 {
 				fmt.Println("目前偏重，吃完饭多散散步，消化消化")
-			}else if fatRate>0.36&&fatRate<=0.41{
+			} else if fatRate > 0.36 && fatRate <= 0.41 {
 				fmt.Println("目前肥胖，开始锻炼锻炼吧～胖砸")
-			}else{
+			} else {
 				fmt.Println("目前严重肥胖！！！管住嘴，迈开腿，开始减肥吧！！！")
 			}
-		}else{
+		} else {
 			fmt.Println("暂时不支持计算未成年人的体脂率～")
 		}
 	}
+	return fatRate
 }
 func main() {
-	var (
-		names [3] string
-		name1 string
-		name2 string
-		name3 string
-		weight [3] float64
-		weight1 float64
-		weight2 float64
-		weight3 float64
-		tall [3] float64
-		tall1 float64
-		tall2 float64
-		tall3 float64
-		age [3]int
-		age1 int
-		age2 int
-		age3 int
-		sex [3]string
-		sex1 string
-		sex2 string
-		sex3 string
-
-	)
-
-	fmt.Print("请输入要计算的姓名（以空格分隔）：")
-	fmt.Scanln(&name1, &name2,&name3)
-	names[0] =name1
-	names[1] =name2
-	names[2] =name3
-	fmt.Print("请输入要计算的体重（以空格分隔）：")
-	fmt.Scanln(&weight1, &weight2,&weight3)
-	weight[0] =weight1
-	weight[1] =weight2
-	weight[2] =weight3
-
-
-	fmt.Print("请输入要计算的身高（以空格分隔）：")
-	fmt.Scanln(&tall1, &tall2,&tall3)
-	tall[0]=tall1
-	tall[1]=tall2
-	tall[2]=tall3
-
-	fmt.Print("请输入要计算的年龄（以空格分隔）：")
-	fmt.Scanln(&age1, &age2,&age3)
-	age[0]=age1
-	age[1]=age2
-	age[2]=age3
-
-	fmt.Print("请输入要计算的性别（以空格分隔）：")
-	fmt.Scanln(&sex1, &sex2,&sex3)
-	sex[0]=sex1
-	sex[1]=sex2
-	sex[2]=sex3
-
-	for i:=0;i<3;i++{
-		var bmi float64 = weight[i] / (tall[i] * tall[i])
-		testFat(names[i],sex[i],bmi,age[i])
+	//获取要计算的人数
+	var count int
+	fmt.Print("请输入要计算的人数：")
+	fmt.Scanln(&count)
+	//终端持续录入
+	for {
+		if count != 0 {
+			//定义存储容器
+			var fatRates []float64
+			for i := 0; i < count; i++ {
+				var (
+					name1   string
+					weight1 float64
+					tall1   float64
+					age1    int
+					sex1    string
+				)
+				//终端获取需要的参数
+				fmt.Printf("请输入第%d个要计算的姓名：", i+1)
+				fmt.Scanln(&name1)
+				fmt.Printf("请输入第%d个要计算的体重(kg)：", i+1)
+				fmt.Scanln(&weight1)
+				fmt.Printf("请输入第%d个要计算的身高(m)：", i+1)
+				fmt.Scanln(&tall1)
+				fmt.Printf("请输入第%d个要计算的年龄：", i+1)
+				fmt.Scanln(&age1)
+				fmt.Printf("请输入第%d个要计算的性别(男/女)：", i+1)
+				fmt.Scanln(&sex1)
+				//计算bmi值
+				var bmi float64 = weight1 / (tall1 * tall1)
+				//计算出体脂率，并存储到slice
+				fatRate := testFat(name1, sex1, bmi, age1)
+				fatRates = append(fatRates, fatRate)
+			}
+			var sum_fat float64
+			//遍历体脂率求和
+			for _, v := range fatRates {
+				sum_fat += v
+			}
+			//计算平均体脂率，输出
+			avg_fatRate := sum_fat / float64(count)
+			fmt.Printf("本次共计算%d个人，平均体脂率是%.2f\n", count, avg_fatRate)
+			return
+		} else {
+			return
+		}
 	}
-
-
-
-
 
 }
--- thirdclass/if_parallel/main.go
+++ thirdclass/if_parallel/main.go
@@ -2,33 +2,39 @@ package main
 
 import "fmt"
 
-func main(){
-	var point1 = [2]float64{1, 1}
-	var point2 =[2]float64{2,2}
-	var line1a float64
-	var line1b float64
-	var line1_b float64
-	line1a = (point1[1]-line1_b)/point1[0]
-	line1b = (point2[1]-line1_b)/point2[0]
-	if line1a ==line1b{
-		fmt.Println("a",line1a)
+func getKofLine(p []float64) float64 {
+	return (p[3] - p[1]) / (p[2] - p[0])
+}
+func main() {
+	for {
+		var point1 []float64
+		//输入第一条直线上的两个点（x1,y1） （x2,y2）
+		for i := 0; i < 2; i++ {
+			var x, y float64
+			fmt.Printf("请输入第一条直线的第%d个点坐标(格式 =>如x y)：", i+1)
+			fmt.Scanln(&x, &y)
+			point1 = append(point1, x, y)
+		}
+		//计算出此直线的斜率
+		line1 := getKofLine(point1)
+		//继续输入第二条直线上的两个点（x1,y1） （x2,y2）
+		var point2 []float64
+		//输入第一条直线上的两个点（x1,y1） （x2,y2）
+		for i := 0; i < 2; i++ {
+			var x, y float64
+			fmt.Printf("请输入第二条直线的第%d个点坐标(格式 =>如x y)：", i+1)
+			fmt.Scanln(&x, &y)
+			point2 = append(point2, x, y)
+		}
+		//计算出第二条直线的斜率
+		line2 := getKofLine(point2)
+		//判断两条直线的斜率是否相等，输出结论
+		if line1 == line2 {
+			fmt.Println("两条直线平行")
+			return
+		}
+		fmt.Println("两条直线不平行")
+		return
 	}
-
-	var point3 = [2]float64{1, 0}
-	var point4 =[2]float64{2,1}
-	var line2a float64
-	var line2b float64
-	var line2_b float64
-
-	line2a = (point3[1]-line2_b)/point3[0]
-	line2b = (point4[1]-line2_b)/point4[0]
-	fmt.Println("b",line2a)
-	fmt.Println("b",line2b)
-
-	if line2a ==line2b{
-		fmt.Println("b",line2a)
-	}
-
-
 
 }
